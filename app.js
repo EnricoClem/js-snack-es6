d4831d8919816d807ee384e6275d553916645a28
EnricoClem <enrico.clem94@gmail.com>
2024-03-20 10:00:50 +0100

    FIX - snack 1 function

--- app.js
+++ app.js
@@ -9,26 +9,72 @@
 // Generiamo e stampiamo in console la lista per i segnaposto.
 
 let vipGuest = [
-    {nome:'Brad Pitt'},
-    {nome:'Johnny Depp'},
-    {nome:'Lady Gaga'},
-    {nome:'Cristiano Ronaldo'},
-    {nome:'Georgina Rodriguez'},
-    {nome:'Chiara Ferragni'},
-    {nome:'Fedez'},
-    {nome:'George Clooney'},
-    {nome:'Amal Clooney'},
-    {nome:'Maneskin'}
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
 ];
 
-let tableSeats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+function toObject(name, order, tableName) {
 
-vipGuest.map(el => Object.assign(el, {tavolo: 'Tavolo Vip'}));
+    return {
+        tableName: tableName,
+        name: name,
+        order: order
+    };
 
-vipGuest.forEach((item, index) => {
-    item.posto = tableSeats[index];
-})
-
-for(let i = 0; i < vipGuest.length; i++) {
-    console.log(vipGuest[i]);
 };
+
+const guestObjects = []
+
+for (let i = 0; i < vipGuest.length; i++) {
+    const guestname = vipGuest[i];
+    const order = i + 1;
+    const guestObject = toObject (guestname, tableName, order);
+    guestObjects.push(guestObject);
+}
+
+console.log(guestObjects)
+
+// _____________________________________________________________________________________________
+
+// *SNACK 2*
+// Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+// 1. Per preparare l’aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo
+// ES (Marco della Rovere => MARCO DELLA ROVERE);
+// 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+// 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+
+let students = [
+    {id: 231,
+    nome: 'Marco della Rovere',
+    voto: 78},
+    {id: 110,
+    nome: 'Paola Cortellessa',
+    voto: 96},
+    {id: 250,
+    nome: 'Andrea Mantegna',
+    voto: 48},
+    {id: 145,
+    nome: 'Gaia Borromini',
+    voto: 74},
+    {id: 196,
+    nome: 'Luigi Grimaldello',
+    voto: 68},
+    {id: 102,
+    nome: 'Piero della Francesca',
+    voto: 50},
+    {id: 120,
+    nome: 'Francesca da Polenta',
+    voto: 84},
+];
+
+let allcapsNames = [];
+
+let allcapsNames =students.map(function)
